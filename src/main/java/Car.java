937bc46e38cee796f13bc6cab8441b037c0d9b15
Belf <sleven182@mail.ru>
2021-04-23 23:19:56 +0300

    add lesson5

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,5 +1,12 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 public class Car implements Runnable{
+    private static boolean win = true;
+    private CyclicBarrier cb;
+    private CountDownLatch cdl1;
+    private CountDownLatch cdl2;
+
     private static int CARS_COUNT;
     static {
         CARS_COUNT = 0;
@@ -13,7 +20,11 @@ public class Car implements Runnable{
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+
+    public Car(Race race, int speed, CyclicBarrier cb, CountDownLatch cdl1, CountDownLatch cdl2) {
+        this.cb = cb;
+        this.cdl1 = cdl1;
+        this.cdl2 = cdl2;
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
@@ -25,11 +36,18 @@ public class Car implements Runnable{
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cdl1.countDown();
+            cb.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
+        }
+        cdl2.countDown();
+        if(win){
+            win = false;
+            System.out.println(name + " Win " + Thread.currentThread().getName());
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,30 @@
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class Main {
     public static final int CARS_COUNT = 4;
     public static void main(String[] args) {
+        CyclicBarrier cb = new CyclicBarrier(CARS_COUNT + 1);
+        final CountDownLatch cdl1 = new CountDownLatch(CARS_COUNT);
+        final CountDownLatch cdl2 = new CountDownLatch(CARS_COUNT);
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Race race = new Race(new Road(60), new Tunnel(CARS_COUNT), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cb, cdl1, cdl2);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        try {
+            cdl1.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            cb.await();
+            cdl2.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
     }
 }
